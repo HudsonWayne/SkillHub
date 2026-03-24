8b5d7dd3c8cecf4fef93e9dce22ea2b2aa6429bd
HudsonWayne <waynewaynebenhura@gmail.com>
2026-03-24 09:02:41 +0200

    initial commit

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -1,3 +1,18 @@
 from rest_framework import serializers
 from users.models import User
 
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["email", "username", "password", "is_service_provider"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data["email"],
+            username=validated_data["username"],
+            password=validated_data["password"],
+            is_service_provider=validated_data.get("is_service_provider", False),
+        )
+        return user
